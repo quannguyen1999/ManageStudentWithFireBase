626d3b43f1e43ac2724a572f8018566e685b0e68
quannguyen1999 <nguyendanganhquan99@gmail.com>
2020-12-12 22:58:47 +0700

    add image

--- app/src/main/java/com/example/myapplication/ClickItemListener.java
+++ app/src/main/java/com/example/myapplication/ClickItemListener.java
@@ -5,4 +5,5 @@ import com.example.myapplication.model.Student;
 //dùng để gửi sự kiện lại qua MainActivity
 public interface ClickItemListener {
     void onItemCLick(Student student, int position);
+
 }
